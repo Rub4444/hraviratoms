b10a2e8166933d94f49487ccbf0a95a5ef5be5f0
Rub4444 <isahakyan06@gmail.com>
2025-11-11 20:24:21 +0400

    33222

--- hraviratoms/resources/views/home.blade.php
+++ hraviratoms/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Հարսանիք</title>
+<title>Ռուբեն & Նոննա</title>
 
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
